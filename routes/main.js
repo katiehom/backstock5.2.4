56d59a866fd2f3b4169117f579c3ecc3f96d9a6e
Katie Hom <schaka01@gmail.com>
2023-03-27 10:06:14 -0600

    Add semicolons

--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/auth') 
-const homeController = require('../controllers/home')
-// const { ensureAuth } = require('../middleware/auth')
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth'); 
+const homeController = require('../controllers/home');
+// const { ensureAuth } = require('../middleware/auth');
 
-router.get('/', homeController.getIndex)
-router.get('/about', homeController.getAbout)
-router.get('/login', authController.getLogin)
-router.post('/login', authController.postLogin)
-router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
-router.post('/signup', authController.postSignup)
-// router.get('/account', ensureAuth, todosController.getAccount)
-// router.put('/account/:id', ensureAuth, todosController.editAccount)
+router.get('/', homeController.getIndex);
+router.get('/about', homeController.getAbout);
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+router.get('/logout', authController.logout);
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
+// router.get('/account', ensureAuth, todosController.getAccount);
+// router.put('/account/:id', ensureAuth, todosController.editAccount);
 
-module.exports = router
+module.exports = router;
