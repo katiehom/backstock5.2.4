caa32cdca8b7d1559424f8560da09cea22dd7289
Katie Hom <schaka01@gmail.com>
2023-03-27 10:03:15 -0600

    Add semicolons

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,8 @@
 module.exports = {
     getIndex: (req,res)=>{
-        res.render('index.ejs')
+        res.render('index.ejs');
     },
     getAbout: (req,res)=>{
-        res.render('about.ejs')
+        res.render('about.ejs');
     }
-}
+};
