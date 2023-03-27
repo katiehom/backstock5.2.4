9138b8b9041561ce13b42f0b297ee815dc298df3
Katie Hom <schaka01@gmail.com>
2023-03-27 10:06:38 -0600

    Add semicolons and remove comment

--- routes/restock.js
+++ routes/restock.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const restockController = require('../controllers/restock') 
-const { ensureAuth } = require('../middleware/auth')
+const express = require('express');
+const router = express.Router();
+const restockController = require('../controllers/restock'); 
+const { ensureAuth } = require('../middleware/auth');
 
-router.get('/', ensureAuth, restockController.getRestock)
-router.get('/addRestock', ensureAuth, restockController.addRestock)
-router.get('/editRestock/:id', ensureAuth, restockController.editRestock)
-router.put('/:id', ensureAuth, restockController.updateRestock)
-router.put('/increaseRestock/:id', ensureAuth, restockController.increaseRestock)
-router.put('/decreaseRestock/:id', ensureAuth, restockController.decreaseRestock)
-router.post('/createRestock', ensureAuth, restockController.createRestock)
-router.post('/exportRestock', ensureAuth, restockController.exportRestock)
-router.delete('/deleteRestock/:id', ensureAuth, restockController.deleteRestock)
+router.get('/', ensureAuth, restockController.getRestock);
+router.get('/addRestock', ensureAuth, restockController.addRestock);
+router.get('/editRestock/:id', ensureAuth, restockController.editRestock);
+router.put('/:id', ensureAuth, restockController.updateRestock);
+router.put('/increaseRestock/:id', ensureAuth, restockController.increaseRestock);
+router.put('/decreaseRestock/:id', ensureAuth, restockController.decreaseRestock);
+router.post('/createRestock', ensureAuth, restockController.createRestock);
+router.post('/exportRestock', ensureAuth, restockController.exportRestock);
+router.delete('/deleteRestock/:id', ensureAuth, restockController.deleteRestock);
 
-module.exports = router
-
-// add ensureAuth to all routes
+module.exports = router;
