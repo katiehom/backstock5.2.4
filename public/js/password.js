05eeac3a7e999c1ec84185bb64bc97e187682c48
Katie Hom <schaka01@gmail.com>
2023-03-27 10:31:59 -0600

    Change double quotes to single

--- public/js/password.js
+++ public/js/password.js
@@ -6,23 +6,23 @@ const signupPassword = document.getElementById('floatingSignupPassword')
 const confirmPassword = document.getElementById('floatingConfirmPassword')
 
 login ? 
-    login?.addEventListener("click", function() {
-        const type = password.getAttribute("type") === "password" ? "text" : "password"
-        password.setAttribute("type", type)
-        login.classList.toggle("fa-eye")
+    login?.addEventListener('click', function() {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password'
+        password.setAttribute('type', type)
+        login.classList.toggle('fa-eye')
 })
 
 : 
-signup?.addEventListener("click", function() {
-    const type = signupPassword.getAttribute("type") === "password" ? "text" : "password"
-    signupPassword.setAttribute("type", type)
-    signup.classList.toggle("fa-eye")
+signup?.addEventListener('click', function() {
+    const type = signupPassword.getAttribute('type') === 'password' ? 'text' : 'password'
+    signupPassword.setAttribute('type', type)
+    signup.classList.toggle('fa-eye')
 })
 
-confirm?.addEventListener("click", function() {
-    const type = confirmPassword.getAttribute("type") === "password" ? "text" : "password"
-    confirmPassword.setAttribute("type", type)
-    confirm.classList.toggle("fa-eye")
+confirm?.addEventListener('click', function() {
+    const type = confirmPassword.getAttribute('type') === 'password' ? 'text' : 'password'
+    confirmPassword.setAttribute('type', type)
+    confirm.classList.toggle('fa-eye')
 })
 
 
