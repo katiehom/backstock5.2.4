5262ce31d5503505ad9acccb48cecd68e3bbe3f7
Katie Hom <schaka01@gmail.com>
2023-03-27 10:05:51 -0600

    Add semicolons and remove comment

--- routes/backstock.js
+++ routes/backstock.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const backstockController = require('../controllers/backstock') 
-const { ensureAuth } = require('../middleware/auth')
+const express = require('express');
+const router = express.Router();
+const backstockController = require('../controllers/backstock');
+const { ensureAuth } = require('../middleware/auth');
 
-router.get('/', ensureAuth, backstockController.getBackstock)
-router.get('/add', ensureAuth, backstockController.add)
-router.get('/edit/:id', ensureAuth, backstockController.edit)
-router.put('/:id', ensureAuth, backstockController.update)
-router.post('/createBackstock', ensureAuth, backstockController.createBackstock)
-router.delete('/deleteBackstock/:id', ensureAuth, backstockController.deleteBackstock)
+router.get('/', ensureAuth, backstockController.getBackstock);
+router.get('/add', ensureAuth, backstockController.add);
+router.get('/edit/:id', ensureAuth, backstockController.edit);
+router.put('/:id', ensureAuth, backstockController.update);
+router.post('/createBackstock', ensureAuth, backstockController.createBackstock);
+router.delete('/deleteBackstock/:id', ensureAuth, backstockController.deleteBackstock);
 
-module.exports = router
-
-// ensure auth for all
+module.exports = router;
